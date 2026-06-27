2d80c1dc9e6182a1003c9457c2a85af9e48cefd5
johnajay17344 <your-email@example.com>
2026-06-27 07:50:44 +0100

    refactore:api

--- src/app/api/github.ts
+++ src/app/api/github.ts
@@ -1,7 +1,9 @@
 import axios from "axios"
 import {GitHubStats,GitHubActivity,GitHubLanguage} from "@/types/github"
 
-const BASE = "https://api.github.com/users/johnajay17344"
+const USERNAME = process.env.GITHUB_USERNAME;
+
+const BASE = `https://api.github.com/users/${USERNAME}`;
 
 const githubAccess = {
   userProfile:  `${BASE}`,
@@ -12,13 +14,16 @@ const githubAccess = {
 
 
 const github = axios.create({
+  baseURL: "https://api.github.com",
+  timeout: 10000,
   headers: {
     Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-     Accept: "application/vnd.github+json",
-    "User-Agent": "NextJS-App",
-  },
+    Accept: "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28",
+}
 })
 console.log(`Bearer ${process.env.GITHUB_TOKEN}`);
+
 
 
 
@@ -37,7 +42,6 @@ export async function fetchStats(): Promise<GitHubStats> {
   const recentCommits = eventsRes.data.filter(
     (e: { type: string }) => e.type === "PushEvent"
   ).length
-
 
   return {
     publicRepos:   profileRes.data.public_repos,
